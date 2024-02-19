085696214fa1bce3548dc7af8c717b63ebc0c232
E7M2NU3L <aemmanuel685210@gmail.com>
2024-02-19 19:53:12 +0530

    made initial changes in the file and folder structure of the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {motion} from 'framer-motion';
+import Home from './components/Home/Home';
+import { Students } from './components/Students/Students';
+import VideoCalls from './components/VideoCalls/VideoCalls';
+import Navbar from './utils/Navbar';
+import Footer from './utils/Footer';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>  
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/students' element={<Students />} />
+          <Route path='/chat' element={<VideoCalls />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </> 
   );
 }
 
---
+++ src/utils/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main.css';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
